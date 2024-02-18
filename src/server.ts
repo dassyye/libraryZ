2ed3d6b1cdfe93bacb8fab2bde0ad7b9093d37d0
dassy <professionaldassy@gmail.com>
2024-02-18 13:38:25 -0300

    docs: docker compose config and connect database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app'
 import 'dotenv/config'
 
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`server running in ${PORT}`)

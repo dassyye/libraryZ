c98015ba688c3134f1c61e7e474b2df8bf8d22a5
dassy <professionaldassy@gmail.com>
2024-02-18 14:39:41 -0300

    docs: create migrations

---
+++ src/db/migrations/20240218164924-bookCategory.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('book_category', {
+      book_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'book',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      category_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'category',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('book_category')
+  }
+};
